04d5047d1cc6baa1e4f778b2d2652d9238b07b05
miguelalmeida0 <94702822+miguelalmeida0@users.noreply.github.com>
2022-03-21 09:47:42 +0100

    Finishing 5.3

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { GiftedChat, Bubble } from "react-native-gifted-chat";
-import { View, Platform, KeyboardAvoidingView, StyleSheet } from "react-native";
+import {
+  View,
+  Platform,
+  Text,
+  KeyboardAvoidingView,
+  StyleSheet,
+} from "react-native";
 import * as firebase from "firebase";
 import "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDzAxXJWaBdKCmcT2yt0ysZYDukX9b2uTI",
@@ -37,6 +44,10 @@ export default class Chat extends React.Component {
     // This will add the name at the top of the screen
     this.props.navigation.setOptions({ title: name });
 
+    this.unsubscribe = this.referenceChatMessages
+      .orderBy("createdAt", "desc")
+      .onSnapshot(this.onCollectionUpdate);
+
     this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
       if (!user) {
         await firebase.auth().signInAnonymously();
@@ -51,10 +62,10 @@ export default class Chat extends React.Component {
           avatar: "https://placeimg.com/140/140/any",
         },
       });
-
-      this.unsubscribe = this.referenceChatMessages
-        .orderBy("createdAt", "desc")
-        .onSnapshot(this.onCollectionUpdate);
+      this.refMsgsUser = firebase
+        .firestore()
+        .collection("messages")
+        .where("uid", "==", this.state.uid);
     });
   }
 
@@ -68,7 +79,11 @@ export default class Chat extends React.Component {
         _id: data._id,
         text: data.text,
         createdAt: data.createdAt.toDate(),
-        user: data.user,
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+          avatar: data.user.avatar,
+        },
       });
     });
     this.setState({
@@ -89,7 +104,20 @@ export default class Chat extends React.Component {
       text: message.text || "",
       createdAt: message.createdAt,
       user: this.state.user,
+      image: message.image || "",
+      location: message.location || null,
     });
+  }
+
+  async deleteMessages() {
+    try {
+      await AsyncStorage.removeItem("messages");
+      this.setState({
+        messages: [],
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 
   getMessages = async () => {
@@ -115,14 +143,32 @@ export default class Chat extends React.Component {
     );
   }
 
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: { backgroundColor: "#59c3c3" },
+        }}
+      />
+    );
+  }
+
   // The view elements look like CSS but they are from React Native!
   render() {
     let { bgColor } = this.props.route.params;
 
     return (
-      <View style={{ backgroundColor: bgColor, flex: 1 }}>
+      <View
+        style={{
+          backgroundColor: bgColor,
+          flex: 1,
+        }}
+      >
         <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
           messages={this.state.messages}
+          user={this.state.user}
           onSend={(messages) => this.onSend(messages)}
           user={{
             _id: this.state.user._id,
@@ -130,6 +176,7 @@ export default class Chat extends React.Component {
             avatar: this.state.user.avatar,
           }}
         />
+
         {Platform.OS === "android" ? (
           <KeyboardAvoidingView behavior="height" />
         ) : null}
